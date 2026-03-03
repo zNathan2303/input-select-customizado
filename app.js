615eb83b2a84ffd176a5dc8ce4d5dbcc0b6d7460
NATHAN DA SILVA COSTA <25132656@CFP127ALUNO.LOCAL>
2026-03-03 08:17:16 -0300

    ajustes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 lucide.createIcons()
 
-let select = document.querySelector('.select'),
-selectedValue = document.getElementById('selected-value'),
-optionsViewButton = document.getElementById('options-view-button'),
-inputsOptions = document.querySelectorAll('.option input')
+const select = document.querySelector('.select'),
+  selectedValue = document.getElementById('selected-value'),
+  optionsViewButton = document.getElementById('options-view-button'),
+  inputsOptions = document.querySelectorAll('.option input')
 
 inputsOptions.forEach(input => {
   input.addEventListener('click', event => {
     selectedValue.textContent = input.dataset.label
 
     const isMouseOrTouch =
-    event.pointerType == 'mouse' ||
-    event.pointerType == 'touch'
+      event.pointerType == 'mouse' ||
+      event.pointerType == 'touch'
 
     isMouseOrTouch && optionsViewButton.click()
   })
@@ -20,7 +20,7 @@ inputsOptions.forEach(input => {
 window.addEventListener('keydown', e => {
   if (!select.classList.contains('open')) return
 
-  if (e.key == 'Escape' || e.key == ' ') {
+  if (e.key == 'Escape' || e.key == ' ' || e.key == 'Enter') {
     optionsViewButton.click()
   }
 })
@@ -31,8 +31,18 @@ optionsViewButton.addEventListener('input', () => {
   if (!select.classList.contains('open')) return
 
   const input =
-  document.querySelector('.option input:checked') ||
-  document.querySelector('.option input')
+    document.querySelector('.option input:checked') ||
+    document.querySelector('.option input')
 
   input.focus()
+})
+
+document.addEventListener('click', event => {
+  if (!select.classList.contains('open')) return
+
+  const options = document.querySelector('#options')
+
+  if (!options.contains(event.target)) {
+    optionsViewButton.click()
+  }
 })
